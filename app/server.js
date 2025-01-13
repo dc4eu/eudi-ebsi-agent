340f75c512a1aedc9cf02a732676065dd10be109
fmerg <fmerg@grnet.gr>
2025-01-13 15:59:11 +0200

    Transfer issuance logic to separate module

--- app/server.js
+++ app/server.js
@@ -1,8 +1,6 @@
 import express from "express";
 import path from "path";
 
-import { v4 as uuidv4 } from "uuid";
-
 import { Resolver } from "did-resolver";
 import { getResolver } from "@cef-ebsi/ebsi-did-resolver";
 
@@ -13,8 +11,6 @@ import { util as utilEbsi } from "@cef-ebsi/ebsi-did-resolver";
 
 import { randomBytes } from "node:crypto";
 
-import { createVerifiableCredentialJwt, verifyCredentialJwt } from "@cef-ebsi/verifiable-credential";
-
 const registry = "https://api-pilot.ebsi.eu/did-registry/v5/identifiers";
 
 const resolverConfig = { registry };
@@ -23,7 +19,7 @@ const didResolver = new Resolver(ebsiResolver);
 
 const app = express();
 
-import { ES256KSigner } from "did-jwt";
+import { issueCredential } from "./vc.js";
 
 
 app.use(express.json());
@@ -67,59 +63,6 @@ async function createDidFromJwk(method, publicJwk) {
   const did = EbsiWallet.createDid(label, publicJwk);
 
   return did;
-}
-
-function base64ToBytes(base64Url) {
-  // Convert to standard base64
-  const normalized = base64Url.replace(/-/g, "+").replace(/_/g, "/");
-  const padding = "=".repeat((4 - (normalized.length % 4)) % 4);
-  const base64Padded = normalized + padding;
-
-  // Decode to binary
-  return Uint8Array.from(Buffer.from(base64Padded, "base64"));
-}
-
-function resolveAlg(jwk) {
-  if (!jwk || !jwk.kty) {
-    throw new Error("Invalid JWK: Missing 'kty' field.");
-  }
-
-  switch (jwk.kty) {
-    case "EC": // Elliptic Curve
-      if (!jwk.crv) {
-        throw new Error("Invalid JWK: Missing 'crv' field for EC key.");
-      }
-      // Resolve EC algorithms based on the curve
-      switch (jwk.crv) {
-        case "secp256k1":
-          return "ES256K"; // ECDSA using secp256k1 and SHA-256
-        case "P-256":
-          return "ES256"; // ECDSA using P-256 and SHA-256
-        case "P-384":
-          return "ES384"; // ECDSA using P-384 and SHA-384
-        case "P-521":
-          return "ES512"; // ECDSA using P-521 and SHA-512
-        default:
-          throw new Error(`Unsupported curve: ${jwk.crv}`);
-      }
-
-    case "RSA": // RSA Keys
-      if (!jwk.n || !jwk.e) {
-        throw new Error("Invalid JWK: Missing 'n' or 'e' field for RSA key.");
-      }
-      // Typically RS256 is a default for RSA
-      return "RS256";
-
-    case "oct": // Symmetric Keys
-      if (!jwk.k) {
-        throw new Error("Invalid JWK: Missing 'k' field for symmetric key.");
-      }
-      // Example: Use HS256 for HMAC with SHA-256
-      return "HS256";
-
-    default:
-      throw new Error(`Unsupported key type: ${jwk.kty}`);
-  }
 }
 
 
@@ -182,7 +125,6 @@ app.get("/issue-credential", async (req, res) => {
   if (!body) {
     return res.status(400).json({ error: "Malformed request: No body" });
   }
-  // TODO: Parse more options here?
   const {
     issuer: issuer_did,
     subject: subject_did,
@@ -190,92 +132,7 @@ app.get("/issue-credential", async (req, res) => {
     kid: issuer_kid,
   } = req.body;
 
-  const now = new Date();
-
-  // valid from 0 seconds from now
-  const validFrom = new Date(now.getTime() + 0 * 60 * 60 * 1000);
-
-  // valid until 10 years from now
-  const validUntil = new Date(now);
-  validUntil.setFullYear(now.getFullYear() + 10);
-
-  // expires at 5 years from now
-  const expirationDate = new Date(now);
-  expirationDate.setFullYear(now.getFullYear() + 5);
-
-  const vcPayload = {
-    "@context": ["https://www.w3.org/2018/credentials/v1"],
-    id: `urn:uuid:${uuidv4()}`,
-    type: ["VerifiableCredential", "VerifiableAttestation"],
-    issuer: issuer_did,
-    issuanceDate: now.toISOString(),
-    issued: now.toISOString(),
-    validFrom: validFrom.toISOString(),
-    validUntil: validUntil.toISOString(),
-    expirationDate: expirationDate.toISOString(),
-    credentialSubject: {
-      // NOTE (GRNET): Must be did:ebsi:<...>due to enabled ebsiAuthority
-      id: subject_did,
-    },
-    credentialSchema: {
-      id: "https://api-pilot.ebsi.eu/trusted-schemas-registry/v3/schemas/z3MgUFUkb722uq4x3dv5yAJmnNmzDFeK5UC8x83QoeLJM",
-      type: "FullJsonSchemaValidator2021",
-    },
-    termsOfUse: {
-      id: `https://api-pilot.ebsi.eu/trusted-issuers-registry/v5/issuers/${issuer_did}/attributes/b40fd9b404418a44d2d9911377a03130dde450eb546c755b5b80acd782902e6d`,
-      type: "IssuanceCertificate",
-    },
-  };
-  const options = {
-    network: "pilot", // Required: EBSI network
-    hosts: ["api-pilot.ebsi.eu"], // Required: List of trusted hosts running the EBSI Core Services APIs.
-    ebsiAuthority: "api-pilot.ebsi.eu", // OPTIONAL; NOTE (GRNET): Enforces did:ebsi:<...> subject DIDs
-    // OPTIONAL. Determines whether to validate the Verifiable Credential payload or not.
-    // Validation is active by default.
-    // NOTE: even when skipValidation is set to true, the payload must be a valid EBSI Verifiable Attestation.
-    // NOTE (GRNET): We deactivate this so that we can work with non-onboarded issuer DIDs
-    skipValidation: true,
-    // OPTIONAL. List of trusted services with their respective version number (e.g. "v5").
-    // Only declare this if you need to override the default versions.
-    // services: {
-    //   "did-registry": "v5",
-    //   "trusted-issuers-registry": "v5",
-    //   "trusted-policies-registry": "v3",
-    //   "trusted-schemas-registry": "v3",
-    // },
-    // OPTIONAL. Timeout after which the requests made by the library will fail. Default: 15 seconds
-    // timeout: 15_000,
-    // OPTIONAL. Determines whether to validate the accreditations of the VC issuer or not.
-    // Validation is active by default.
-    // skipAccreditationsValidation: false,
-    // OPTIONAL. Determines whether to validate the credential status or not.
-    // Validation is active by default.
-    // skipStatusValidation: false,
-    // OPTIONAL. Determines whether to validate the credential subject or not
-    // Validation is active by default.
-    // skipCredentialSubjectValidation: false,
-    // OPTIONAL. Unix timestamp. Optional comparison date. Default: current date and time.
-    // validAt: Date.now(),
-    // OPTIONAL. Credential subject. This parameter is mandatory if the payload's `credentialSubject` is an array.
-    // It must correspond to one of the IDs in the payload's `credentialSubject` array.
-    // sub: "did:ebsi:z25a23eWUxQQzmAgnD9srpMM",
-    // OPTIONAL. Enable Ajv verbose mode (default: false)
-    // verbose: false,
-    // OPTIONAL. Extra credentialSchema types. By default, the library only supports "FullJsonSchemaValidator2021" and "JsonSchema".
-    // The library is not responsible for validating these extra types.
-    // extraCredentialSchemaTypes: [],
-  };
-
-
-  // TODO: Resolve signer type per alg
-  const signer = ES256KSigner(base64ToBytes(issuer_jwk["d"]));
-  const issuer = {
-      did: issuer_did,
-      kid: issuer_kid,
-      alg: resolveAlg(issuer_jwk),
-      signer,
-  };
-  const vcJwt = await createVerifiableCredentialJwt(vcPayload, issuer, options);
+  const vcJwt = await issueCredential(issuer_jwk, issuer_did, issuer_kid, subject_did);
   res.json({ vcJwt });
 });
 
---
+++ app/vc.js
@@ -0,0 +1,94 @@
+import { createVerifiableCredentialJwt, verifyCredentialJwt } from "@cef-ebsi/verifiable-credential";
+import { v4 as uuidv4 } from "uuid";
+import { ES256KSigner } from "did-jwt";
+import { base64ToBytes, resolveAlgorithm } from "./util.js";
+
+
+export async function issueCredential(issuer_jwk, issuer_did, issuer_kid, subject_did) {
+  const now = new Date();
+
+  // valid from 0 seconds from now
+  const validFrom = new Date(now.getTime() + 0 * 60 * 60 * 1000);
+
+  // valid until 10 years from now
+  const validUntil = new Date(now);
+  validUntil.setFullYear(now.getFullYear() + 10);
+
+  // expires at 5 years from now
+  const expirationDate = new Date(now);
+  expirationDate.setFullYear(now.getFullYear() + 5);
+
+  const vcPayload = {
+    "@context": ["https://www.w3.org/2018/credentials/v1"],
+    id: `urn:uuid:${uuidv4()}`,
+    type: ["VerifiableCredential", "VerifiableAttestation"],
+    issuer: issuer_did,
+    issuanceDate: now.toISOString(),
+    issued: now.toISOString(),
+    validFrom: validFrom.toISOString(),
+    validUntil: validUntil.toISOString(),
+    expirationDate: expirationDate.toISOString(),
+    credentialSubject: {
+      // NOTE (GRNET): Must be did:ebsi:<...>due to enabled ebsiAuthority
+      id: subject_did,
+    },
+    credentialSchema: {
+      id: "https://api-pilot.ebsi.eu/trusted-schemas-registry/v3/schemas/z3MgUFUkb722uq4x3dv5yAJmnNmzDFeK5UC8x83QoeLJM",
+      type: "FullJsonSchemaValidator2021",
+    },
+    termsOfUse: {
+      id: `https://api-pilot.ebsi.eu/trusted-issuers-registry/v5/issuers/${issuer_did}/attributes/b40fd9b404418a44d2d9911377a03130dde450eb546c755b5b80acd782902e6d`,
+      type: "IssuanceCertificate",
+    },
+  };
+  const options = {
+    network: "pilot", // Required: EBSI network
+    hosts: ["api-pilot.ebsi.eu"], // Required: List of trusted hosts running the EBSI Core Services APIs.
+    ebsiAuthority: "api-pilot.ebsi.eu", // OPTIONAL; NOTE (GRNET): Enforces did:ebsi:<...> subject DIDs
+    // OPTIONAL. Determines whether to validate the Verifiable Credential payload or not.
+    // Validation is active by default.
+    // NOTE: even when skipValidation is set to true, the payload must be a valid EBSI Verifiable Attestation.
+    // NOTE (GRNET): We deactivate this so that we can work with non-onboarded issuer DIDs
+    skipValidation: true,
+    // OPTIONAL. List of trusted services with their respective version number (e.g. "v5").
+    // Only declare this if you need to override the default versions.
+    // services: {
+    //   "did-registry": "v5",
+    //   "trusted-issuers-registry": "v5",
+    //   "trusted-policies-registry": "v3",
+    //   "trusted-schemas-registry": "v3",
+    // },
+    // OPTIONAL. Timeout after which the requests made by the library will fail. Default: 15 seconds
+    // timeout: 15_000,
+    // OPTIONAL. Determines whether to validate the accreditations of the VC issuer or not.
+    // Validation is active by default.
+    // skipAccreditationsValidation: false,
+    // OPTIONAL. Determines whether to validate the credential status or not.
+    // Validation is active by default.
+    // skipStatusValidation: false,
+    // OPTIONAL. Determines whether to validate the credential subject or not
+    // Validation is active by default.
+    // skipCredentialSubjectValidation: false,
+    // OPTIONAL. Unix timestamp. Optional comparison date. Default: current date and time.
+    // validAt: Date.now(),
+    // OPTIONAL. Credential subject. This parameter is mandatory if the payload's `credentialSubject` is an array.
+    // It must correspond to one of the IDs in the payload's `credentialSubject` array.
+    // sub: "did:ebsi:z25a23eWUxQQzmAgnD9srpMM",
+    // OPTIONAL. Enable Ajv verbose mode (default: false)
+    // verbose: false,
+    // OPTIONAL. Extra credentialSchema types. By default, the library only supports "FullJsonSchemaValidator2021" and "JsonSchema".
+    // The library is not responsible for validating these extra types.
+    // extraCredentialSchemaTypes: [],
+  };
+
+
+  // TODO: Resolve signer type per alg
+  const signer = ES256KSigner(base64ToBytes(issuer_jwk["d"]));
+  const issuer = {
+      did: issuer_did,
+      kid: issuer_kid,
+      alg: resolveAlgorithm(issuer_jwk),
+      signer,
+  };
+  return createVerifiableCredentialJwt(vcPayload, issuer, options);
+}
